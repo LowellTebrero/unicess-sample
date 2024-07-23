d4c202d79d8cae1f44a9643ab322f9743c7ff4c5
LowellTebrero <lowelltebrero@gmail.com>
2024-07-23 15:31:58 +0800

    update nanaman

--- src/components/Program/ProgramServices.vue
+++ src/components/Program/ProgramServices.vue
@@ -2,62 +2,62 @@
     
     <section class="bg-blue-800" id="program">
         <h1 class="text-xl lg:text-2xl font-semibold tracking-wide text-white py-7 lg:py-10 text-center">PROGRAM AND SERVICES</h1>
-     <div class="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4  2xl:h-[70vh] gap-2 just mx-auto w-[90%] lg:w-[70%] p-4">
+     <div class="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4  2xl:h-[70vh] gap-4 just mx-auto w-[90%] lg:w-[70%] p-4">
         <div class="bg-white h-auto xl:h-[28vh] flex flex-col items-center justify-center p-5  lg:py-10 rounded-lg gap-y-4 text-center">
-            <img src="/src/assets/img/running.png" alt="" class="w-[3.4rem] lg:w-[4rem]"  width="70">
+            <img src="/src/assets/img/running.png" alt="" class="w-[3.4rem] xl:w-[4rem]"  width="70">
             <div>
-                <h1 class="text-blue-500 font-semibold text-xs md:text-sm lg:text-lg">Physical Fitness & Sport Develpment</h1>
+                <h1 class="text-blue-500 font-semibold text-xs md:text-sm xl:text-md 2xl:text-base">Physical Fitness & Sport Develpment</h1>
             </div>
-            <button class="text-white bg-yellow-500 rounded-lg py-1 px-2 md:px-3 text-xs md:text-sm lg:text-base">Show Details</button>
+            <button class="text-white bg-yellow-500 rounded-lg py-1 px-2 md:px-3 text-xs md:text-sm xl:text-md 2xl:text-base">Show Details</button>
         </div>
         <div class="bg-white h-auto xl:h-[28vh] flex flex-col items-center justify-center p-5  lg:py-10 rounded-lg gap-y-4 text-center">
-            <img src="/src/assets/img/ps-2.png" alt="" class="w-[3.4rem] lg:w-[4rem]"  width="70">
+            <img src="/src/assets/img/ps-2.png" alt="" class="w-[3.4rem] xl:w-[4rem]"  width="70">
             <div>
-                <h1 class="text-blue-500 font-semibold text-xs md:text-sm lg:text-lg">Information Communication & Education</h1>
+                <h1 class="text-blue-500 font-semibold text-xs md:text-sm xl:text-md 2xl:text-base">Information Communication & Education</h1>
             </div>
-            <button class="text-white bg-yellow-500 rounded-lg py-1 px-2 md:px-3 text-xs md:text-sm lg:text-base">Show Details</button>
+            <button class="text-white bg-yellow-500 rounded-lg py-1 px-2 md:px-3 text-xs md:text-sm xl:text-md 2xl:text-base">Show Details</button>
         </div>
         <div class="bg-white h-auto xl:h-[28vh] flex flex-col items-center justify-center p-5  lg:py-10 rounded-lg gap-y-4 text-center">
-            <img src="/src/assets/img/ps-3.png" alt="" class="w-[3.4rem] lg:w-[4rem]"  width="70">
+            <img src="/src/assets/img/ps-3.png" alt="" class="w-[3.4rem] xl:w-[4rem]"  width="70">
             <div>
-                <h1 class="text-blue-500 font-semibold text-xs md:text-sm lg:text-lg">Literacy, Numeracy & Language Enhancement</h1>
+                <h1 class="text-blue-500 font-semibold text-xs md:text-sm xl:text-md 2xl:text-base">Literacy, Numeracy & Language Enhancement</h1>
             </div>
-            <button class="text-white bg-yellow-500 rounded-lg py-1 px-2 md:px-3 text-xs md:text-sm lg:text-base">Show Details</button>
+            <button class="text-white bg-yellow-500 rounded-lg py-1 px-2 md:px-3 text-xs md:text-sm xl:text-md 2xl:text-base">Show Details</button>
         </div>
         <div class="bg-white h-auto xl:h-[28vh] flex flex-col items-center justify-center p-5  lg:py-10 rounded-lg gap-y-4 text-center">
-            <img src="/src/assets/img/ps-4.png" alt="" class="w-[3.4rem] lg:w-[4rem]"  width="70">
+            <img src="/src/assets/img/ps-4.png" alt="" class="w-[3.4rem] xl:w-[4rem]"  width="70">
             <div>
-                <h1 class="text-blue-500 font-semibold text-xs md:text-sm lg:text-lg">Cultural Development</h1>
+                <h1 class="text-blue-500 font-semibold text-xs md:text-sm xl:text-md 2xl:text-base">Cultural Development</h1>
             </div>
-            <button class="text-white bg-yellow-500 rounded-lg py-1 px-2 md:px-3 text-xs md:text-sm lg:text-base">Show Details</button>
+            <button class="text-white bg-yellow-500 rounded-lg py-1 px-2 md:px-3 text-xs md:text-sm xl:text-md 2xl:text-base">Show Details</button>
         </div>
         <div class="bg-white h-auto xl:h-[28vh] flex flex-col items-center justify-center p-5  lg:py-10 rounded-lg gap-y-4 text-center">
-            <img src="/src/assets/img/ps-5.png" alt="" class="w-[3.4rem] lg:w-[4rem]"  width="70">
+            <img src="/src/assets/img/ps-5.png" alt="" class="w-[3.4rem] xl:w-[4rem]"  width="70">
             <div>
-                <h1 class="text-blue-500 font-semibold text-xs md:text-sm lg:text-lg">Livelihood, Technical and Business Management</h1>
+                <h1 class="text-blue-500 font-semibold text-xs md:text-sm xl:text-md 2xl:text-base">Livelihood, Technical and Business Management</h1>
             </div>
-            <button class="text-white bg-yellow-500 rounded-lg py-1 px-2 md:px-3 text-xs md:text-sm lg:text-base">Show Details</button>
+            <button class="text-white bg-yellow-500 rounded-lg py-1 px-2 md:px-3 text-xs md:text-sm xl:text-md 2xl:text-base">Show Details</button>
         </div>
         <div class="bg-white h-auto xl:h-[28vh] flex flex-col items-center justify-center p-5  lg:py-10 rounded-lg gap-y-4 text-center">
-            <img src="/src/assets/img/ps-6.png" alt="" class="w-[3.4rem] lg:w-[4rem]"  width="70">
+            <img src="/src/assets/img/ps-6.png" alt="" class="w-[3.4rem] xl:w-[4rem]"  width="70">
             <div>
-                <h1 class="text-blue-500 font-semibold text-xs md:text-sm lg:text-lg">Environmental Conservation & Disaster Preparedness</h1>
+                <h1 class="text-blue-500 font-semibold text-xs md:text-sm xl:text-md 2xl:text-base">Environmental Conservation & Disaster Preparedness</h1>
             </div>
-            <button class="text-white bg-yellow-500 rounded-lg py-1 px-2 md:px-3 text-xs md:text-sm lg:text-base">Show Details</button>
+            <button class="text-white bg-yellow-500 rounded-lg py-1 px-2 md:px-3 text-xs md:text-sm xl:text-md 2xl:text-base">Show Details</button>
         </div>
         <div class="bg-white h-auto xl:h-[28vh] flex flex-col items-center justify-center p-5  lg:py-10 rounded-lg gap-y-4 text-center">
-            <img src="/src/assets/img/ps-7.png" alt="" class="w-[3.4rem] lg:w-[4rem]"  width="70">
+            <img src="/src/assets/img/ps-7.png" alt="" class="w-[3.4rem] xl:w-[4rem]"  width="70">
             <div>
-                <h1 class="text-blue-500 font-semibold text-xs md:text-sm lg:text-lg">Management & Leadership Development</h1>
+                <h1 class="text-blue-500 font-semibold text-xs md:text-sm xl:text-md 2xl:text-base">Management & Leadership Development</h1>
             </div>
-            <button class="text-white bg-yellow-500 rounded-lg py-1 px-2 md:px-3 text-xs md:text-sm lg:text-base">Show Details</button>
+            <button class="text-white bg-yellow-500 rounded-lg py-1 px-2 md:px-3 text-xs md:text-sm xl:text-md 2xl:text-base">Show Details</button>
         </div>
         <div class="bg-white h-auto xl:h-[28vh] flex flex-col items-center justify-center p-5  lg:py-10 rounded-lg gap-y-4 text-center">
-            <img src="/src/assets/img/ps-8.png" alt="" class="w-[3.4rem] lg:w-[4rem]"  width="70">
+            <img src="/src/assets/img/ps-8.png" alt="" class="w-[3.4rem] xl:w-[4rem]"  width="70">
             <div>
-                <h1 class="text-blue-500 font-semibold text-xs md:text-sm lg:text-lg">Especial Institute & Teaching Training Program</h1>
+                <h1 class="text-blue-500 font-semibold text-xs md:text-sm xl:text-md 2xl:text-base">Especial Institute & Teaching Training Program</h1>
             </div>
-            <button class="text-white bg-yellow-500 rounded-lg py-1 px-2 md:px-3 text-xs md:text-sm lg:text-base">Show Details</button>
+            <button class="text-white bg-yellow-500 rounded-lg py-1 px-2 md:px-3 text-xs md:text-sm xl:text-md 2xl:text-base">Show Details</button>
         </div>
        
         
